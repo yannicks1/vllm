e0beefed08240fdd471f6bbf79ee3d09333799b5
Yannick Schnider <Yannick.Schnider1@ibm.com>
2025-02-14 14:37:46 +0000

    using with pytest.raises instead of try and except
    Signed-off-by: Yannick Schnider <Yannick.Schnider1@ibm.com>

--- tests/plugins/vllm_add_dummy_scheduler/vllm_add_dummy_scheduler/dummy_scheduler.py
+++ tests/plugins/vllm_add_dummy_scheduler/vllm_add_dummy_scheduler/dummy_scheduler.py
@@ -3,11 +3,7 @@
 from vllm.core.scheduler import Scheduler
 
 
-class CustomException(Exception):
-    pass
-
-
 class DummyScheduler(Scheduler):
 
     def schedule(self):
-        raise CustomException("Exception raised by DummyScheduler")
+        raise Exception("Exception raised by DummyScheduler")
--- tests/plugins_tests/test_scheduler_plugins.py
+++ tests/plugins_tests/test_scheduler_plugins.py
@@ -5,13 +5,14 @@ def test_scheduler_plugins():
     # simulate workload by running an example
     import runpy
 
-    from vllm_add_dummy_scheduler.dummy_scheduler import CustomException
+    import pytest
+
     current_file = __file__
     import os
     example_file = os.path.join(
         os.path.dirname(os.path.dirname(os.path.dirname(current_file))),
         "examples", "offline_inference/basic.py")
-    try:
+
+    with pytest.raises(Exception) as exception_info:
         runpy.run_path(example_file)
-    except CustomException as e:
-        assert str(e) == "Exception raised by DummyScheduler"
+    assert str(exception_info.value) == "Exception raised by DummyScheduler"
